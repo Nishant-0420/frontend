c5166fc6020686c417d0affb89a28be114f0f1ac
NISHANT <thakurnishant0420@gmail.com>
2024-02-25 18:38:35 +0530

    starting with nextjs and using reactjs

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,14 @@
 // ctrl+a to select all 
-// rafce 
+// rafce component structure
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>home</div>
+    <div>
+      <h1></h1>
+      <img src="https://assets.editorial.aetnd.com/uploads/2017/09/halloween-folklore-devil-getty-173469109.jpg?width=768" alt="" />
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus labore tempora, fugiat quos voluptates nobis doloremque maiores dolorum eum fugit. Reiciendis quibusdam vero ratione fuga.</p>
+    </div>
   )
 }
 

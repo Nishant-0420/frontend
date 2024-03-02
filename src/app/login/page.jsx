a589331d2a5e56643edcc9256a49d96aeec99fdb
NISHANT <thakurnishant0420@gmail.com>
2024-03-02 20:02:48 +0530

    loginpage

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -72,7 +72,7 @@ function Login() {
                           </a>
                         </div>
                         <div className="d-flex align-items-center justify-content-center pb-4">
-                          <p className="mb-0 me-2">Don't have an account?</p>
+                          <p className="mb-0 me-2">Don't have an account ?</p>
                           <button
                             type="button"
                             className="btn btn-outline-danger"

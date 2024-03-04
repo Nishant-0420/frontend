c7d554b2b35e60402bf47e44c2e55ec039e3ada7
NISHANT <thakurnishant0420@gmail.com>
2024-03-04 18:40:42 +0530

    todolist

--- src/app/todolist/page.jsx
+++ src/app/todolist/page.jsx
@@ -5,14 +5,32 @@ const TodoList = () => {
     // to show updates on page we use state way  useState() to create variable and useState only returns 2value
     // first variable is used to read and 2nd one is function to update value
     // when fun returns multiple value then use array like const [a,b,c]
-    let [taskList, updateTaskList]= useState([
-        {text:'do homework', completed: false},
-        {text:'learn react', completed: false},
-        {text:'getting high at night', completed: false}
-    ])
+    let [taskList, updateTaskList]= useState([])
     console.log(taskList);
     // console.log(updateTaskList);
-    const addTask=(e)=>{ console.log(e.key)}
+    const addTask=(e)=>{ 
+        // code give name of key
+        if(e.code==='Enter' && 'Tap'){
+            console.log(e.target.value);
+            const obj={text: e.target.value , completed:false}
+            // ... to remove outer array
+            updateTaskList([obj,...taskList])
+            e.target.value = '';
+        }
+    }
+
+    const deleteTask=(index)=>{
+        
+        const temp=taskList;
+        temp.splice(index,1)
+        updateTaskList([...temp])
+    }
+    const completeTask=(index)=>{
+        const temp=taskList;
+        temp[index].completed= !temp[index].completed;
+        updateTaskList([...temp]);
+        console.log(temp)
+    }
 
   return (
     <div className='bg-success vh-100 '>
@@ -22,7 +40,7 @@ const TodoList = () => {
             <div className="card shadow">
                 <div className="card-header">
                     <input 
-                    // onKeyDown()
+                    // onKeyDown() tell which key is pressed
                     onKeyDown={addTask}
                     type="text" className='form-control border-2 border-success'
                     placeholder='add your task here' />
@@ -30,10 +48,19 @@ const TodoList = () => {
                 <div className="card-body">
                     {
                         // taskList.map(n=> <h1>{n.text}</h1> )
+                        // when passing some parameter use call back fun
                         taskList.map((n,index)=> 
-                        <div className='p-4 shadow d-flex justify-content-between mb-2' key={index}>
+                        <div className='p-4 shadow d-flex justify-content-between mb-2 align-items-center' key={index}>
+                            <input type="checkbox"  checked={n.completed}/>
                             <p>{n.text}</p>
-                            <button className='btn btn-success'> Delete</button>
+                            {n.completed ? 
+                            <span className='badge bg-primary'>Completed</span>
+                            :
+                            <span className='badge bg-warning'>Pending</span> }
+                        
+                            <button onClick={()=>{deleteTask(index) }} className='btn btn-danger'> Delete</button>
+                            <button onClick={()=>{completeTask(index)}} className='btn btn-success'>
+                            {n.completed ? 'undo': 'complete task'}</button>
                             </div> )
                         
                     }
